6eba0c95ad252550ff7e917a37fb6e0ed97a6713
Zhihao Li <abelli578294267@gmail.com>
2020-05-22 20:38:24 +0100

    add home-page and navigation bar

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ from .forms import PostForm
 
 
 # Create your views here.
+def blog_home(request):
+    return render(request, 'blog/home.html')
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
